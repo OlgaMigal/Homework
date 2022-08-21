b906495b4757eae00f439bb074542b2f45b16469
Olga_Migal <Tashaloriya@gmail.com>
2022-08-22 01:18:22 +0300

    Task 8 done

---
+++ Hometask1_8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N (включительно).
+// Сделала только на положительные числа. На отрицательные
+// тоже можно, но не стала.
+Console.Write("Введите неотрицательное число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+if (number>1)
+{
+while (count<=number)
+{
+Console.Write($"{count} ");
+count = count+2;
+}
+}
+else Console.Write("Введённое число слишком мало");
+Console.WriteLine();
