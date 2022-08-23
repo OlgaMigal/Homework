e3e4882be4aa9b6dc0786d0acf899e1a9b831a7d
Olga_Migal <Tashaloriya@gmail.com>
2022-08-23 23:38:40 +0300

    Hometask2_10 пока без проверки трёхзначности числа

---
+++ Hometask2_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int Second(int num)
+{
+    int number = num % 100 / 10;
+    return number;
+}
+Console.Write("Введите трёхзначное число: ");
+// int inter = int.Parse(Console.ReadLine());
+// int number = Second(inter);
+
+int number = Second(int.Parse(Console.ReadLine()));
+Console.WriteLine("Средняя цифра = " + number);
