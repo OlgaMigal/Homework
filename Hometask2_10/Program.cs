30474fbd62edbd68d129a662b9297920dda28934
Olga_Migal <Tashaloriya@gmail.com>
2022-08-24 21:53:07 +0300

    Hometask2_10 с проверкой трёхзначности

--- Hometask2_10/Program.cs
+++ Hometask2_10/Program.cs
@@ -4,14 +4,25 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+// не стала мучиться с проверкой корректности ввода
 int Second(int num)
 {
     int number = num % 100 / 10;
     return number;
 }
-Console.Write("Введите трёхзначное число: ");
-// int inter = int.Parse(Console.ReadLine());
-// int number = Second(inter);
 
-int number = Second(int.Parse(Console.ReadLine()));
-Console.WriteLine("Средняя цифра = " + number);
+Console.Write("Введите трёхзначное число: ");
+
+//int number = Second(int.Parse(Console.ReadLine())); - короче, 
+//но непригодно для проверки трёхзначности
+
+//для проверки трёхзначности числа ввела дополнительную переменную inter:
+int inter = int.Parse(Console.ReadLine());
+int number = Second(inter);
+
+if (inter / 100 > 0 & inter / 100 < 10)
+{
+    Console.WriteLine("Средняя цифра = " + number);
+}
+else Console.WriteLine("Ошибка ввода, перезапустите программу");
