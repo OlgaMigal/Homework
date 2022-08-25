2ac8fb1b14640912595911d43c7d36ab67b0435b
Olga_Migal <Tashaloriya@gmail.com>
2022-08-25 22:37:16 +0300

    Hometask2_15 сделала через switch

---
+++ Hometask2_15/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+// Попробовала через конструкцию switch, потому что раньше через неё не пробовала
+
+Console.Write("Введите номер дня недели: ");
+int num = int.Parse(Console.ReadLine());
+switch (num)
+{
+    case 1:
+        Console.WriteLine("будний день");
+        break;
+    case 2:
+        Console.WriteLine("будний день");
+        break;
+    case 3:
+        Console.WriteLine("будний день");
+        break;
+    case 4:
+        Console.WriteLine("будний день");
+        break;
+    case 5:
+        Console.WriteLine("будний день");
+        break;
+    case 6:
+        Console.WriteLine("выходной");
+        break;
+    case 7:
+        Console.WriteLine("выходной");
+        break;
+    default:
+        Console.WriteLine("Под этим номером нет дня недели");
+        break;
+}
