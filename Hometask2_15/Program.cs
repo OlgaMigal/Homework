30153d4d65f9b4a0ed4c25e5ba1daf81241e3614
Olga_Migal <Tashaloriya@gmail.com>
2022-08-28 22:01:19 +0300

    Hometask2_15 изменила switch на короткий вариант

--- Hometask2_15/Program.cs
+++ Hometask2_15/Program.cs
@@ -9,23 +9,13 @@ int num = int.Parse(Console.ReadLine());
 switch (num)
 {
     case 1:
-        Console.WriteLine("будний день");
-        break;
     case 2:
-        Console.WriteLine("будний день");
-        break;
     case 3:
-        Console.WriteLine("будний день");
-        break;
     case 4:
-        Console.WriteLine("будний день");
-        break;
     case 5:
         Console.WriteLine("будний день");
         break;
     case 6:
-        Console.WriteLine("выходной");
-        break;
     case 7:
         Console.WriteLine("выходной");
         break;
