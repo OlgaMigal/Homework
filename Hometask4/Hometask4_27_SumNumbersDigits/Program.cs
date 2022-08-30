6da541892c3132d81a320f272ba182589fd8f000
Olga_Migal <Tashaloriya@gmail.com>
2022-08-30 22:50:38 +0300

    Hometask4_27_SumNumbersDigits done

---
+++ Hometask4/Hometask4_27_SumNumbersDigits/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход 
+//число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+string NumbersDigitsSum(int num)
+{
+    if(num<0)
+    {
+        return "Err";
+    }
+    else
+    {
+        int i = 0;
+        int result = 0;
+        while(num>0)
+        {
+            result+=num%10;
+            num/=10;
+            i+=1;
+        }
+        return $"Cумма цифр введённого числа = {result}";
+    }
+}
+
+Console.WriteLine("Введите положительное число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(NumbersDigitsSum(number));
